d19aeffde6804ee3234ccc10d6bfeda42a8f2acb
Arquiescalta <juanjov020113@gmail.com>
2023-02-24 21:36:02 -0500

    arreglado

--- MAINMENU.js
+++ MAINMENU.js
@@ -1,16 +1,36 @@
-/*
+// elementById para redirigir
 let dichos = document.getElementById("dichos");
 dichos.addEventListener("click", fdichos);
 
 let estereotipos = document.getElementById("estereotipos");
 estereotipos.addEventListener("click", festereotipos);
 
-let farandula = document.getElementById("Farandula");
+let farandula = document.getElementById("farandula");
 farandula.addEventListener("click", ffarandula);
 
-let infancia = document.getElementById("Infancia");
+let infancia = document.getElementById("infancia");
 infancia.addEventListener("click", finfancia); 
-*/
+
+// Funciones para redirigir
+function fdichos(event) 
+{
+  location.href = "/DICHOS.HTML"
+}
+
+function festereotipos(event) 
+{
+  location.href = "/ESTEREOTIPOS.HTML"
+}
+
+function ffarandula(event) 
+{
+  location.href = "/FARANDULA.HTML"
+}
+
+function finfancia(event) 
+{
+  location.href = "/INFANCIA.HTML"
+}
 
 // Constantes para tipo de charada
 const arrayDichos = ['sapo', 'gonorrea', 'hp', 'marica'];
