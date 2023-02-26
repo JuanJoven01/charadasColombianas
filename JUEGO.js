05d5d0e076e79fc4a3c134447caee2c2d075bd60
Arquiescalta <juanjov020113@gmail.com>
2023-02-26 16:44:55 -0500

    Muestra palabras

--- JUEGO.js
+++ JUEGO.js
@@ -1,55 +1,31 @@
+let writeCorrect = document.getElementById("correcto");
+writeCorrect.addEventListener("click", nextDicho);
+
+let writeIncorrect = document.getElementById("incorrecto");
+writeIncorrect.addEventListener("click", nextDicho);
+
+let wordSelect = document.getElementById("modificable")
+
 // Constantes para tipo de charada
 const arrayDichos = ['sapo', 'gonorrea', 'hp', 'marica'];
 const arrayEstereotipos = ['rolo', 'caleno', 'cucuteno', 'paisa'];
 const arrayFarandula = ['maluma', 'shakira', 'diomedes', 'juanes'];
 const arrayInfancia = ['ArtAttack', 'MandySenorbigotes', 'dalmatas', 'princesasapo'];
-/*
-
-// elementById para correcto e incorrecto
-let correcto = document.getElementById('correcto')
-correcto.addEventListener('click', fcorrecto)
-
-
- funciones correcto e incorrecto
-
-function fcorrecto(event) 
-{
-    const azarIndex = Math.floor(Math.random() * event.length);
-    return event[azarIndex];
-} 
-let solodichos = fcorrecto(arrayDichos);
-console.log(solodichos);
-
-*/
 
 // Funciones para el arrey 
 function seleccionarDichos(array) 
 {
-  const azarIndex = Math.floor(Math.random() * array.length);
-  return array[azarIndex];
+    const azarIndex = Math.floor(Math.random() * array.length);
+    return array[azarIndex];   
 }
 
-function seleccionarEstereotipos(array) 
+function nextDicho(event) 
 {
-  const azarIndex = Math.floor(Math.random() * array.length);
-  return array[azarIndex];
+  if (event.click = true)
+      {
+        seleccionarDichos(arrayDichos)
+        var y = seleccionarDichos(arrayDichos)
+        wordSelect.innerHTML = y
+      }     
 }
 
-function seleccionarFarandula(array) 
-{
-  const azarIndex = Math.floor(Math.random() * array.length);
-  return array[azarIndex];
-}
-
-function seleccionarInfancia(array) 
-{
-  const azarIndex = Math.floor(Math.random() * array.length);
-  return array[azarIndex];
-}
-
-// Console para ver funcion
-let adivinarDichos = seleccionarDichos(arrayDichos)+ ", " +
-seleccionarEstereotipos(arrayEstereotipos)+ ", " +
-seleccionarFarandula(arrayFarandula)+ ", " +
-seleccionarInfancia(arrayInfancia);
-console.log(adivinarDichos);
