490e76932ec7533b4b3048c9ed9822122af1d61b
Arquiescalta <juanjov020113@gmail.com>
2023-02-26 19:50:22 -0500

    Contadores y diferencias todo arreglado

--- JUEGO.js
+++ JUEGO.js
@@ -3,29 +3,26 @@
 let writeCorrect = document.getElementById("correcto");
 writeCorrect.addEventListener("click", nextDicho);
 writeCorrect.addEventListener("click", fcountCorrect);
+writeCorrect.addEventListener("click", fcountResult);
 
 let writeIncorrect = document.getElementById("incorrecto");
 writeIncorrect.addEventListener("click", nextDicho);
 writeIncorrect.addEventListener("click", fcountIncorrect);
+writeIncorrect.addEventListener("click", fcountResult);
 
 let wordSelect = document.getElementById("modificable")
 
-let getcountCorrect = document.getElementById("corr");
+let getCountCorrect = document.getElementById("corr");
 
+let getCountIncorrect = document.getElementById("inco");
 
-let getcountIncorrect = document.getElementById("inco");
+let getCountResult = document.getElementById("resultado");
 
-let countCorrect = 1;
+let countCorrect = 0;
 
-let countIncorrect = 1;
+let countIncorrect = 0;
 
 let countResult;
-
-let more;
-
-let less;
-
-
 
 // Constantes para tipo de charada
 const arrayDichos = ['sapo', 'gonorrea', 'hp', 'marica'];
@@ -53,26 +50,28 @@ function nextDicho(event)
 
 // Contador de eventos correctos e incorrectos
 
-function fcountCorrect(sumaContador) 
+function fcountCorrect() 
 {
   sumaContador = countCorrect++;  
-  getcountCorrect.innerHTML = sumaContador;
+  getCountCorrect.innerHTML = sumaContador + 1;
 }
 
-function fcountIncorrect(restaContador) 
+function fcountIncorrect() 
 {
   restaContador = countIncorrect++;  
-  getcountIncorrect.innerHTML = restaContador;
+  getCountIncorrect.innerHTML = restaContador + 1;
 }
 
-function fDifference(sumaContador, restaContador) 
-{
-  return sumaContador - restaContador;
-}
 function fcountResult()
 {
-  let more = sumaContador;
-  let less = restaContador;
-  let result = fDifference(more, less);
-  console.log(result)
+  let more = countCorrect;
+  let less = countIncorrect;
+  let countResult = more - less
+  if (countResult < 0)
+  {
+    countResult = 0
+  }
+  getCountResult.innerHTML = countResult;
+
 }
+
