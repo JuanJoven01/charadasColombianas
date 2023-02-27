15d79a785c5271c267b6f6c0a8ea8e9c646fc768
Arquiescalta <juanjov020113@gmail.com>
2023-02-27 15:26:35 -0500

    Funcion que espera a funcion
    Se hizo que la función launcher la cual inicia el juego, esperara hasta que el contador de segundos llegara a cero para permitir el inicio del juego

--- JUEGO.js
+++ JUEGO.js
@@ -1,86 +1,28 @@
-// get element and event 
-
-let writeCorrect = document.getElementById("correcto");
-writeCorrect.addEventListener("click", nextDicho);
-writeCorrect.addEventListener("click", fcountCorrect);
-writeCorrect.addEventListener("click", fcountResult);
-
-let writeIncorrect = document.getElementById("incorrecto");
-writeIncorrect.addEventListener("click", nextDicho);
-writeIncorrect.addEventListener("click", fcountIncorrect);
-writeIncorrect.addEventListener("click", fcountResult);
-
+// Get element
 let wordSelect = document.getElementById("modificable");
-
-let launch = document.getElementById("iniciar");
-launch.addEventListener("click", TimerCount);
-launch.addEventListener("click", nextDicho);
-
 let getCountCorrect = document.getElementById("corr");
-
 let getCountIncorrect = document.getElementById("inco");
-
 let getCountResult = document.getElementById("resultado");
-
 let getCountTime = document.getElementById("temporizador");
 
+// Variables
 let countCorrect = 0;
-
 let countIncorrect = 0;
-
 let countResult;
+let seconds = 5;
+let launch = document.getElementById("iniciar");
+let writeCorrect = document.getElementById("correcto");
+let writeIncorrect = document.getElementById("incorrecto");
 
-let seconds = 3;
+// Event listener launch
+
+launch.addEventListener("click", TimerCount);
 
 // Constantes para tipo de charada
 const arrayDichos = ['sapo', 'gonorrea', 'hp', 'marica'];
 const arrayEstereotipos = ['rolo', 'caleno', 'cucuteno', 'paisa'];
 const arrayFarandula = ['maluma', 'shakira', 'diomedes', 'juanes'];
 const arrayInfancia = ['ArtAttack', 'MandySenorbigotes', 'dalmatas', 'princesasapo'];
-
-// Funciones para el arrey 
-function seleccionarDichos(array) 
-{
-    const azarIndex = Math.floor(Math.random() * array.length);
-    return array[azarIndex];   
-}
-
-// Funciones para event
-function nextDicho(event) 
-{
-  if (event = true)
-      {
-        seleccionarDichos(arrayDichos)
-        let y = seleccionarDichos(arrayDichos)
-        wordSelect.innerHTML = y
-      }     
-}
-
-// Contador de eventos correctos e incorrectos
-
-function fcountCorrect() 
-{
-  sumaContador = countCorrect++;  
-  getCountCorrect.innerHTML = sumaContador + 1;
-}
-
-function fcountIncorrect() 
-{
-  restaContador = countIncorrect++;  
-  getCountIncorrect.innerHTML = restaContador + 1;
-}
-
-function fcountResult()
-{
-  let more = countCorrect;
-  let less = countIncorrect;
-  let countResult = more - less
-  if (countResult < 0)
-  {
-    countResult = 0
-  }
-  getCountResult.innerHTML = countResult;
-}
 
 // funcion de contador
 function TimerCount()
@@ -95,8 +37,76 @@ function TimerCount()
     if (seconds === 0) 
     {
     clearInterval(temporizador);
-    console.log("Tiempo finalizado");
     }
     }, 1000
   );
 }
+
+// Funciones para el arrey 
+function seleccionarDichos(array) 
+{
+    const azarIndex = Math.floor(Math.random() * array.length);
+    return array[azarIndex];   
+}
+
+// Funciones para event
+function nextDicho(event) 
+{
+  if (event = true)
+      {
+        seleccionarDichos(arrayDichos)
+        let words = seleccionarDichos(arrayDichos)
+        wordSelect.innerHTML = words
+      }     
+}
+
+  // function launcher
+function launcher(seconds) 
+{
+  writeCorrect.addEventListener("click", nextDicho);
+  writeCorrect.addEventListener("click", fcountCorrect);
+  writeCorrect.addEventListener("click", fcountResult);
+  
+  writeIncorrect.addEventListener("click", nextDicho);
+  writeIncorrect.addEventListener("click", fcountIncorrect);
+  writeIncorrect.addEventListener("click", fcountResult);
+  
+  // Contador de eventos correctos e incorrectos
+function fcountCorrect() 
+  {
+    sumaContador = countCorrect++;  
+    getCountCorrect.innerHTML = sumaContador + 1;
+  }
+  
+  function fcountIncorrect() 
+  {
+    restaContador = countIncorrect++;  
+    getCountIncorrect.innerHTML = restaContador + 1;
+  }
+  
+  function fcountResult()
+  {
+    let more = countCorrect;
+    let less = countIncorrect;
+    let countResult = more - less
+    if (countResult < 0)
+    {
+      countResult = 0
+    }
+    getCountResult.innerHTML = countResult;
+  }
+}
+
+// Funcion waitexcecute
+const waitexcecute = () => {
+  if (seconds > 0) {
+    setTimeout(waitexcecute, 1000); // espera 1 segundo y vuelve a comprobar
+    return;
+  }
+  else
+  {  
+    launcher();
+    nextDicho();  
+  }
+}
+waitexcecute();
