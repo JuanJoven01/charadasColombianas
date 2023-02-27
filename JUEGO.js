98e5f047520358513e9583c9bc3a6bd7b663cfe8
Arquiescalta <juanjov020113@gmail.com>
2023-02-27 16:17:47 -0500

    Contandor de juego
    Tiempo de juego agragado

--- JUEGO.js
+++ JUEGO.js
@@ -4,12 +4,14 @@ let getCountCorrect = document.getElementById("corr");
 let getCountIncorrect = document.getElementById("inco");
 let getCountResult = document.getElementById("resultado");
 let getCountTime = document.getElementById("temporizador");
+let getTimeCount = document.getElementById('tiempojuego')
 
 // Variables
 let countCorrect = 0;
 let countIncorrect = 0;
 let countResult;
 let seconds = 5;
+let playTime = 60;
 let launch = document.getElementById("iniciar");
 let writeCorrect = document.getElementById("correcto");
 let writeIncorrect = document.getElementById("incorrecto");
@@ -33,8 +35,27 @@ function TimerCount()
     {
     seconds--;
     getCountTime.innerHTML = seconds;
-    console.log(seconds);
-    if (seconds === 0) 
+    if (seconds == 0) 
+    {
+    clearInterval(temporizador);
+    }
+    if (seconds < 0) 
+    {
+      seconds = 1;
+    }
+    }, 1000
+  );
+}
+
+function playcount()
+{
+  let temporizador = setInterval
+  (
+    function timer() 
+    {
+    playTime--;
+    getTimeCount.innerHTML = playTime;
+    if (playTime == 0) 
     {
     clearInterval(temporizador);
     }
@@ -98,15 +119,19 @@ function fcountCorrect()
 }
 
 // Funcion waitexcecute
-const waitexcecute = () => {
+const waitexcecute = () => 
+{
   if (seconds > 0) {
     setTimeout(waitexcecute, 1000); // espera 1 segundo y vuelve a comprobar
     return;
   }
   else
   {  
+    playcount();
     launcher();
     nextDicho();  
   }
+
 }
 waitexcecute();
+
